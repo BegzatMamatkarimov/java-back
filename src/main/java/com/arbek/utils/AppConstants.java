4d03743472a4813a5ef60e148261559ef3d231ab
arshadiyarov <shadiyarovarmat@gmail.com>
2024-05-01 17:18:17 +0500

    fix: small fix

--- src/main/java/com/arbek/utils/AppConstants.java
+++ src/main/java/com/arbek/utils/AppConstants.java
@@ -4,7 +4,7 @@ public class AppConstants {
   public static final String PAGE_NUMBER = "0";
   public static final String PAGE_SIZE = "10";
 
-  public static final String SORT_BY = "movieId";
+  public static final String SORT_BY = "genres";
 
-  public static final String SORT_DIR = "asc";
+  public static final String SORT_DIR = "desc";
 }
